51502fd8d864a1228094987dc323774c1e10ac09
Jake H-P <72385707+jhpyke@users.noreply.github.com>
2023-11-17 10:28:33 +0000

    Initial implementation of new pull_push buckets (#66)
    * Initial implementation of new pull_push buckets
    
    * Fix linting error

--- data_engineering_exports/pull.py
+++ data_engineering_exports/pull.py
@@ -28,33 +28,69 @@ def create_pull_bucket_policy(args: Dict[str, str]) -> AwaitableGetPolicyDocumen
     """
     bucket_arn = args.pop("bucket_arn")
     pull_arns = args.pop("pull_arns")
-
-    bucket_policy = get_policy_document(
-        statements=[
-            GetPolicyDocumentStatementArgs(
-                actions=[
-                    "s3:GetObject",
-                    "s3:GetObjectAcl",
-                    "s3:GetObjectVersion",
-                ],
-                principals=[
-                    GetPolicyDocumentStatementPrincipalArgs(
-                        identifiers=pull_arns, type="AWS"
-                    )
-                ],
-                resources=[bucket_arn + "/*"],
-            ),
-            GetPolicyDocumentStatementArgs(
-                actions=["s3:ListBucket"],
-                principals=[
-                    GetPolicyDocumentStatementPrincipalArgs(
-                        identifiers=pull_arns, type="AWS"
-                    )
-                ],
-                resources=[bucket_arn],
-            ),
-        ]
-    )
+    allow_push = args.pop("allow_push", False)
+    writable_actions = [
+        "s3:GetObject",
+        "s3:GetObjectAcl",
+        "s3:GetObjectVersion",
+        "s3:DeleteObject",
+        "s3:DeleteObjectVersion",
+        "s3:PutObject",
+        "s3:PutObjectAcl",
+        "s3:PutObjectTagging",
+        "s3:RestoreObject",
+    ]
+    standard_actions = [
+        "s3:GetObject",
+        "s3:GetObjectAcl",
+        "s3:GetObjectVersion",
+    ]
+    if allow_push:
+        bucket_policy = get_policy_document(
+            statements=[
+                GetPolicyDocumentStatementArgs(
+                    actions=writable_actions,
+                    principals=[
+                        GetPolicyDocumentStatementPrincipalArgs(
+                            identifiers=pull_arns, type="AWS"
+                        )
+                    ],
+                    resources=[bucket_arn + "/*"],
+                ),
+                GetPolicyDocumentStatementArgs(
+                    actions=["s3:ListBucket"],
+                    principals=[
+                        GetPolicyDocumentStatementPrincipalArgs(
+                            identifiers=pull_arns, type="AWS"
+                        )
+                    ],
+                    resources=[bucket_arn],
+                ),
+            ]
+        )
+    else:
+        bucket_policy = get_policy_document(
+            statements=[
+                GetPolicyDocumentStatementArgs(
+                    actions=standard_actions,
+                    principals=[
+                        GetPolicyDocumentStatementPrincipalArgs(
+                            identifiers=pull_arns, type="AWS"
+                        )
+                    ],
+                    resources=[bucket_arn + "/*"],
+                ),
+                GetPolicyDocumentStatementArgs(
+                    actions=["s3:ListBucket"],
+                    principals=[
+                        GetPolicyDocumentStatementPrincipalArgs(
+                            identifiers=pull_arns, type="AWS"
+                        )
+                    ],
+                    resources=[bucket_arn],
+                ),
+            ]
+        )
     return bucket_policy
 
 
